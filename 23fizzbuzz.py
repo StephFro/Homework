acaf03a61ae76f9a24b902a27bcde4a26b34bce1
StephFro <stephiecbf@gmail.com>
2023-02-04 20:46:00 -0800

    new

--- 23fizzbuzz.py
+++ 23fizzbuzz.py
@@ -10,9 +10,9 @@
 for i in range(1, 101):
 	if i % 15 == 0:
 		print('FizzBuzz')
-	if i % 3 == 0:
+	elif i % 3 == 0 and i % 15 != 0:
 		print('Fizz')
-	elif i % 5 == 0:
+	elif i % 5 == 0 and i % 15 != 0:
 		print('Buzz')
 	else:
 		print(i)
