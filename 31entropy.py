35db61e7f52114444a0cab2cad754535f773f7df
StephFro <stephiecbf@gmail.com>
2023-02-07 21:06:42 -0800

    new

--- 31entropy.py
+++ 31entropy.py
@@ -11,6 +11,39 @@
 # Hint: try breaking your program with erroneous input
 
 
+#import library for command line inputs and math
+import sys
+import math
+
+#create list without leading title, intialize variables
+list = sys.argv[1:]
+tot = 0
+
+for i in range(len(list)):
+	#transform each element to a int
+	list[i] = float(list[i])
+	
+
+for i in range(len(list)):
+	#sum the list for a test later
+	tot += list[i]
+	
+#in case of numbers not summing to 1.0
+if tot != 1:
+	print('Probabilites do not sum to 1.0. Please try again.')
+else:
+	H = 0
+	for i in range(len(list)):
+		#the entropy calculation for each element in the list
+		H = H + -(list[i] * math.log(list[i],2))
+	print(f'{H:.4}')
+
+
+
+
+
+
+
 """
 python3 31entropy.py 0.1 0.2 0.3 0.4
 1.846
