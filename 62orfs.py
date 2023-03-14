85d010e2cb73809a3f0237095c3e9316142d2cbf
stephanie <stephiecbf@gmail.com>
2023-03-14 00:23:20 -0700

    new

--- 62orfs.py
+++ 62orfs.py
@@ -16,6 +16,76 @@
 # Note: your genes should be similar to those in the real genome
 
 
+import mcb185
+import argparse
+
+parser = argparse.ArgumentParser(description ='ORF')
+
+parser.add_argument('file', type=str, metavar='<path>', help='fileish')
+parser.add_argument('--orf', type=int, default=300, metavar='<int>', help='orf mini size')
+	
+arg = parser.parse_args()
+
+#example to test with
+example = 'AGCGAGCGAGAGTTGCGATAGCGATAGCTATGATCGGAGAA'
+
+#make a funtion that reverses DNA
+def reverse(gene):
+	revSeq = ''
+	for i in range(len(gene) - 1, -1, -1):
+		if gene[i] == 'A': revSeq += 'T'
+		elif gene[i] == 'C': revSeq += 'G'
+		elif gene[i] == 'G': revSeq += 'C'
+		elif gene[i] == 'T': revSeq += 'A'
+		else: revSeq += 'X'
+	return revSeq
+	
+def revComp(gene):
+	return parent[::-1]
+	
+def comp(gene):
+	revSeq = ''
+	for i in range(len(parent) -1, -1, -1):
+		if parent[i] == 'A': revSeq += 'T'
+		elif gene[i] == 'C': revSeq += 'G'
+		elif gene[i] == 'G': revSeq += 'C'
+		elif gene[i] == 'T': revSeq += 'A'
+		else: revSeq += 'X'
+	return rseq[::-1]
+	
+def orf(seq, size = 0, rev = False):
+	if rev: seq = reverse(seq)
+	
+	for i in range(3):
+		while i <= len(seq) - 3:
+			codon = seq[i:i+3]
+			
+			if codon == 'AUG':
+				for j in range(i+3, len(seq) - 2, 3):
+					codon = seq[j:j+3]
+					if codon == 'TAA' or codon == 'TAG' or codon == 'TGA':
+						if j - i > size:
+							if rev: 
+								yield len(seq)-j-2, len(seq) - i, mcb185.translate(seq[i:j+2])
+							else:
+								yield i+1, j+3, mcb185.translate(seq[i:j+2])
+						i = j
+						break
+			i +=3
+			
+for defline, seq in mcb185.read_fasta(arg.file):
+	word = defline.split()
+	name = word[0]
+	revSeq = reverse(seq[:1])
+	for begin, end, pro in orf(seq, arg.orf):
+		print(name, begin, end, '+', pro[:10])
+		
+	for revBegin, revEnd, rPro in orf(seq, arg.orf, True):
+		print(name, revBegin, revEnd, '-', rPro[:10])
+		
+
+		
+
 """
 python3 62orfs.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz
 NC_000913.3 108 500 - MVFSIIATRW
