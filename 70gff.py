cafcdcb595d76ddcf8347d3ba8a85d1925146131
stephanie <stephiecbf@gmail.com>
2023-03-18 21:15:54 -0700

    new

--- 70gff.py
+++ 70gff.py
@@ -4,6 +4,47 @@
 # Use the E. coli genome gff
 # Your code should mimic the output below
 
+import json
+import argparse
+import gzip
+import re
+
+parser = argparse.ArgumentParser(description='GFF -> json')
+parser.add_argument('file',type=str, metavar= '<path>', help='GFF')
+arg = parser.parse_args()
+
+genome = []
+
+with gzip.open(arg.file, 'rt') as fp:
+	for line in fp:
+		if line.startswith('#'): continue
+		for matches in re.finditer('RefSeq\sgene', line):
+			all = {}
+			
+			#do the search
+			coordinate = re.search('(\d+)\s(\d+)', line)
+			begin = int(coordinate.group(1))
+			ending = int(coordinate.group(2))
+			
+			st = re.search('\.\s(.)\s\.', line)
+			strand = st.group(1)
+			
+			name = re.search('Name=(\w+)', line)
+			gene = str(name.group(1))
+			
+			#dictionary
+			all['gene'] = gene
+			all['begin'] = begin
+			all['ending'] = ending
+			all['strand'] = strand
+			
+			genome.append(all)
+			
+print(json.dumps(genome, indent=4))
+
+
+
+
 
 """
 python3 70gff.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.gff.gz
--- 71gff.py
+++ 71gff.py
@@ -10,6 +10,62 @@
 
 # Note: gene names are stored differently here than the last file
 
+import argparse
+import json
+import re
+import gzip
+
+parser = argparse.ArgumentParser(description='GFF -> JSON')
+parser.add_argument('file', type=str, metavar='<path>', help='GFF')
+arg = parser.parse_args()
+
+
+chromosome = {}
+genome = {}
+
+with gzip.open(arg.file, 'rt') as fp:
+	for line in fp:
+		for match in re.finditer('\s(gene)\s',line):
+			
+			#Chromosome Counter
+			f = line.split()
+			chromo = f[0]
+			if chromo not in chromosome: 
+				chromosome[chromo] =0
+			chromosome[chromo] += 1
+		
+			#Coordinate Search
+			coor = re.search('\s(\d+)\s(\d+)\s',line)
+			begin = int(coor.group(1))
+			ending = int(coor.group(2))
+			
+			#Name Search
+			name = re.search('sequence_name=(\w+\.\w+)',line)
+			gene = str(name.group(1))
+			
+			#Strand Search
+			st = re.search('\.\s(.)\s\.',line)
+			strand = st.group(1)
+			
+			#Gene Details
+			all = {}
+			
+			all["gene"] = gene
+			all["begin"] = begin
+			all["ending"] = ending
+			all["strand"] = strand
+			
+			#Adding to Genome Database
+			if chromo not in genome:
+				genome[chromo] = []
+			
+			genome[chromo].append(all)
+
+
+for item in chromosome:
+	print(item, chromosome[item])
+
+print(json.dumps(genome, indent=4))
 
 """
 python3 71gff.py elegans
